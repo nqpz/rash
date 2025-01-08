41c27fd2d56917e7ba5ba31ad000230aa71c3518
Niels G. W. Serup <ngws@metanohi.name>
2025-01-08 22:04:10 +0100

    Inline the rest of Runner into Main

--- src/Main.hs
+++ src/Main.hs
@@ -5,10 +5,12 @@ import qualified System.Exit as Exit
 import qualified System.Directory as Dir
 import qualified System.FilePath as Path
 import qualified Data.List as L
+import qualified Data.Text as T
 
 import Rash.StateDirGetter (getOrCreateStateDir)
 import qualified Rash.Representation.Internal as RI
-import Rash.Runner (runFile)
+import Rash.Interpreter (interpret)
+import Rash.IOStateKeeping (retrieveState)
 
 main :: IO ()
 main = do
@@ -17,7 +19,14 @@ main = do
     (fname : readArgs) -> do
       stateDir <- getOrCreateStateDir
       paths <- rashPaths stateDir fname
-      runFile (RI.WriteAndReadFiles paths) fname (L.intercalate " " readArgs)
+      let ioStateKeeping = RI.WriteAndReadFiles paths
+          readArgs' = L.intercalate " " readArgs
+      (asm, state) <- retrieveState fname readArgs' ioStateKeeping
+      let context = RI.Context { RI.contextAssembly = asm
+                               , RI.contextReadArgs = T.pack readArgs'
+                               , RI.contextIOStateKeeping = ioStateKeeping
+                               }
+      interpret context state
     [] -> Exit.exitFailure
 
 rashPaths :: FilePath -> FilePath -> IO RI.RashPaths
