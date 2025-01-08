5daa2b9c4db2f2ab4449d4617bd53413b4e351d7
Niels G. W. Serup <ngws@metanohi.name>
2025-01-08 22:53:14 +0100

    Add test

--- test/InterpreterTests.hs
+++ test/InterpreterTests.hs
@@ -2,8 +2,32 @@ module InterpreterTests
   ( test
   ) where
 
--- import qualified Rash.Representation.Internal as RI
--- import Rash.Interpreter (interpret)
+import Control.Monad (when)
+import Data.IORef (newIORef, readIORef, writeIORef, modifyIORef)
+
+import Paths_rash (getDataFileName)
+
+import qualified Rash.Representation.Internal as RI
+import qualified Rash.Interpreter as Interp
 
 test :: IO ()
-test = pure ()
+test = do
+  fname <- getDataFileName "test/programs/add.rash"
+  asmMRef <- newIORef Nothing
+  stateMRef <- newIORef Nothing
+  linesRef <- newIORef []
+  let ioStateKeeping = RI.InMemory asmMRef stateMRef (\line -> modifyIORef linesRef (line :))
+      interpret args = Interp.interpret fname args ioStateKeeping
+      check expected = do
+        actualReversed <- readIORef linesRef
+        let actual = reverse actualReversed
+        when (actual /= expected)
+          $ fail ("Expected " ++ show expected ++ " but got " ++ show actual)
+        writeIORef linesRef []
+
+  interpret []
+  check ["Please enter the first number.\n"]
+  interpret ["4"]
+  check ["Please enter the second number.\n"]
+  interpret ["91"]
+  check ["4 + 91 = 95\n"]
