e5e17fe7fc2454e0fe241300a49ac577ee3695b2
Niels G. W. Serup <ngws@metanohi.name>
2025-01-08 22:14:22 +0100

    Slightly refactor Main

--- src/Main.hs
+++ src/Main.hs
@@ -5,8 +5,8 @@ import qualified System.Exit as Exit
 import qualified System.Directory as Dir
 import qualified System.FilePath as Path
 
-import Rash.StateDirGetter (getOrCreateStateDir)
 import qualified Rash.Representation.Internal as RI
+import Rash.StateDirGetter (getOrCreateStateDir)
 import Rash.Interpreter (interpret)
 
 main :: IO ()
@@ -14,13 +14,12 @@ main = do
   args <- Env.getArgs
   case args of
     (fname : readArgs) -> do
-      stateDir <- getOrCreateStateDir
-      paths <- rashPaths stateDir fname
+      paths <- rashPaths fname =<< getOrCreateStateDir
       interpret fname readArgs $ RI.WriteAndReadFiles paths
     [] -> Exit.exitFailure
 
 rashPaths :: FilePath -> FilePath -> IO RI.RashPaths
-rashPaths stateDir fname = do
+rashPaths fname stateDir = do
   fnameCanon <- Dir.canonicalizePath fname
   let fnameSave = stateDir ++ fnameCanon
       dirSave = fst $ Path.splitFileName fnameSave
