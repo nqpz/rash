191fbd1dc7ef6b6fa2556795dfb0b24e1d730b3c
Niels G. W. Serup <ngws@metanohi.name>
2025-01-26 14:53:43 +0100

    Add guess-number example program

--- test/InterpreterTests.hs
+++ test/InterpreterTests.hs
@@ -44,5 +44,33 @@ addTest = testCase "add" $ do
   interpret ["91"]
   check ["4 + 91 = 95\n"]
 
+guessNumberTest :: TestTree
+guessNumberTest = testCase "guess-number" $ do
+  (interpret, getActual, check) <- makeTester "examples/guess-number.rash"
+  let guess :: Int -> Int -> IO ()
+      guess start end = do
+        let n = start + (end - start) `div` 2
+        interpret [show n]
+        actual <- getActual
+        -- Ignore the empty outputs of running 'test'.
+        let response = case reverse actual of
+              echo_output : _ -> echo_output
+              _ -> error ("Unexpected output: " ++ show actual)
+        case response of
+          "Your guess was too low, try again.\n" ->
+            guess (n + 1) end
+          "Your guess was too high, try again.\n" ->
+            guess start (n - 1)
+          "Congratulations, you guessed the number!\n" ->
+            pure ()
+          _ -> error ("Unexpected response:" ++ show response)
+
+  interpret []
+  check ["Guess the number!\n"]
+  guess 1 100
+
 tests :: TestTree
-tests = testGroup "Interpreter tests" [addTest]
+tests = testGroup "Interpreter tests"
+  [ addTest
+  , guessNumberTest
+  ]
