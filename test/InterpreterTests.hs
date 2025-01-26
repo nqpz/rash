9f54675bae157202a9cf9eebcd5d22ccd8414336
Niels G. W. Serup <ngws@metanohi.name>
2025-01-26 14:31:07 +0100

    Generalize add test

--- test/InterpreterTests.hs
+++ test/InterpreterTests.hs
@@ -13,21 +13,29 @@ import qualified Rash.Interpreter as Interp
 import Test.Tasty
 import Test.Tasty.HUnit
 
-addTest :: TestTree
-addTest =
-  testCase "add" $ do
-  fname <- getDataFileName "test/programs/add.rash"
+makeTester :: FilePath -> IO ([String] -> IO (),
+                              IO [String],
+                              [String] -> IO ())
+makeTester path = do
+  fname <- getDataFileName path
   asmMRef <- newIORef Nothing
   stateMRef <- newIORef Nothing
   linesRef <- newIORef []
   let ioStateKeeping = RI.InMemory asmMRef stateMRef (\line -> modifyIORef linesRef (line :))
-      interpret args = Interp.interpret fname args ioStateKeeping
-      check expected = do
-        actualReversed <- readIORef linesRef
-        let actual = reverse actualReversed
-        when (actual /= expected)
-          $ fail ("Expected " ++ show expected ++ " but got " ++ show actual)
+      interpret args = do
         writeIORef linesRef []
+        Interp.interpret fname args ioStateKeeping
+      getActual = do
+        actualReversed <- readIORef linesRef
+        pure $ reverse actualReversed
+      check expected = do
+        actual <- getActual
+        when (actual /= expected) $ fail ("Expected " ++ show expected ++ " but got " ++ show actual)
+  pure (interpret, getActual, check)
+
+addTest :: TestTree
+addTest = testCase "add" $ do
+  (interpret, _, check) <- makeTester "examples/add.rash"
 
   interpret []
   check ["Please enter the first number.\n"]
